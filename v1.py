da6b8c0af6e0c1335201cfe042b65acecab95c30
Kristina Kudryavtseva <chkud03@gmail.com>
2022-11-14 17:42:32 +0100

    Repo init commit

---
+++ v1.py
@@ -0,0 +1,7 @@
+""" Add a comment paragraph at the top of each source code file
+    providing the minimal authentication and description of your SPOP product.
+    Date, Time, Author(s),
+    Product name,
+    Product general description,
+    File content description,..
+"""
